6f489fd0bf8cdd05d92ae68f4fceb1b4c82c54a4
olesgedz <gedzoles@gmail.com>
2020-02-14 07:26:33 +0300

    model

--- test/main.cpp
+++ test/main.cpp
@@ -59,9 +59,9 @@ int main(int argc, char **argv)
 		{
 			Vec3f v0 = model->vert(face[j]);
 			Vec3f v1 = model->vert(face[(j + 1) % 3]);
-			int x0 = (v0.x + 1.) * width / 2.;
+			int x0 = (v0.x + 1.) * width  / 2.;
 			int y0 = (v0.y + 1.) * height / 2.;
-			int x1 = (v1.x + 1.) * width / 2.;
+			int x1 = (v1.x + 1.) * width  / 2.;
 			int y1 = (v1.y + 1.) * height / 2.;
 			line(x0, y0, x1, y1, image, white);
 		}
